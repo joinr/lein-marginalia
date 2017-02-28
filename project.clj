5f3c7b375ffababc18295cf75705931d3e186ce5
tom <tom@that-place>
2017-02-28 05:00:20 -0600

    Changed to snapshot temporarily.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject joinr/lein-marginalia "0.9.1"
+(defproject joinr/lein-marginalia "0.9.1-SNAPSHOT"
   :description "Leiningen plugin for Marginalia."
   :url "http://github.com/gdeer81/marginalia"
-  :dependencies [[joinr/marginalia "0.9.1"]]
+  :dependencies [[joinr/marginalia "0.9.1-SNAPSHOT"]]
   :eval-in :leiningen)
 
